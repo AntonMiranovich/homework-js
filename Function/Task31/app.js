a7321086f00b7761295f4a822d1f7bac45915153
AntonMiranovich <antonmir992@gmail.com>
2024-06-23 23:46:25 +0300

    add task

--- Function/Task31/app.js
+++ Function/Task31/app.js
@@ -2,6 +2,36 @@
 // встречается чаще всего.
 
 
+// const checkLength = (arr) => {
+//     let result = {
+//         title: '',
+//         count: 0
+//     }
+
+//     for (let i = 0; i < arr.length; i++) {
+//         let tampalaruObj = {
+//             title: arr[i],
+//             count: 0
+//         }
+
+//         for (let a = 0; a < arr.length; a++) {
+//             if (arr[a] === tampalaruObj.title) {
+//                 tampalaruObj.count += 1
+//             }
+//         }
+
+//         if (tampalaruObj.count > result.count) {
+//             result = tampalaruObj
+//         }
+//     }
+
+//     return `Строка \'${result.title}\' повториласть ${result.count} раз(а)`
+// }
+
+// console.log(checkLength(['anton', 'an', 'anton', 'an', 'an']));
+
+
+
 const checkLength = (arr) => {
     let result = {
         title: '',
@@ -9,23 +39,16 @@ const checkLength = (arr) => {
     }
 
     for (let i = 0; i < arr.length; i++) {
-        let tampalaruObj = {
-            title: arr[i],
-            count: 0
-        }
-
-        for (let a = 0; a < arr.length; a++) {
-            if (arr[a] === tampalaruObj.title) {
-                tampalaruObj.count +=1
+        const newArr = arr.filter((el) => arr[i] !== el)
+        if(result.count<arr.length-newArr.length){
+            result={
+                title:arr[i],
+                count:arr.length-newArr.length
             }
-        }
-
-        if (tampalaruObj.count > result.count) {
-            result = tampalaruObj
         }
     }
 
-    return result
+    return `Строка \'${result.title}\' повториласть ${result.count} раз(а)`
 }
 
 console.log(checkLength(['anton', 'an', 'anton', 'an', 'an']));
--- Function/Task7/app.js
+++ Function/Task7/app.js
@@ -1,3 +1,6 @@
 // 7. Напишите функцию, которая принимает массив строк и возвращает массив только тех
 // строк, которые содержат больше пяти символов.
 
+const filterArray = (arr) => arr.filter((el) => el.length > 5)
+
+console.log(filterArray(['antonn', 'asd', 'asdsadasdasd', 'dasda']));
